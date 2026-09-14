d9eec0b2c6b53a40daaec26e649ceee871853759
Mahak Singh <mahaksinghh17@gmail.com>
2026-09-14 11:08:49 +0530

    Assignment 3

---
+++ assignment3/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+// Serve HTML and CSS files
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`Portfolio running at http://localhost:${PORT}`);
+});
